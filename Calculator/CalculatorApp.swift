dcd67c85aa91a06134d3a587e283fd4fd77066c3
luisorrantia <alberto.oala68@gmail.com>
2024-07-17 20:49:49 -0400

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Luis Orrantia on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
